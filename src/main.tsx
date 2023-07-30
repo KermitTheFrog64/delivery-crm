f0aac065d457285bbbf3026b903c8d910f9d82eb
KermitTheFrog64 <vincent1890@mail.ru>
2023-07-30 10:54:26 +0300

    add console log

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client"
 import { store, App } from "./app"
 import { Provider } from "react-redux"
 
+console.log( 'for gh/dev' );
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
